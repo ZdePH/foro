32091fe16bb99f073282ca981c9bab644fe523d7
Zacarías de Prado Habela <zdeph@MacBook-de-Zacarias.local>
2017-12-28 16:39:32 +0100

    correcion error faltaba ; en use datanase...

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,26 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    // crea y elimina el usuario de la BD
+    use DatabaseTransactions;
+
     /**
      * A basic functional test example.
      *
      * @return void
      */
+    
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $name = 'Duilio Palacios';
+
+        $user = factory(\App\User::class)->create([
+
+        'name' => $name]);
+
+        $this->actingAs($user,'api');
+
+        $this->visit('api/user')
+             ->see('Duilio Palacios');
     }
 }
