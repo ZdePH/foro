5970f0aad653141da30b9d004ceca678dc1524eb
ZdePH <zacarias@formatica.es>
2017-12-28 22:12:10 +0100

    no message

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
 
     'aliases' => [
 
-        'App' => Illuminate\Support\Facades\App::class,
+        'App' => Illuminate\Support\Facades\App::class, 
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
